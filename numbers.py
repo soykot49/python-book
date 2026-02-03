b8ba6690e3ffd1dac424e44c92d665ee72302382
soykot49 <mdrezwannurrashid26@gmail.com>
2026-02-03 09:52:08 +0600

    new

--- numbers.py
+++ numbers.py
@@ -12,3 +12,10 @@ z = 2 + 5j	# complex
 print(type(x))  
 print(type(y))
 print(type(z))  
+
+# You can convert from one type to another with the int(), float(), and complex() methods:
+
+#here how to generate randim number using random
+
+import random
+print(random.randint(1,10))
