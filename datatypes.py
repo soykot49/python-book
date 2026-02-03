cf4ad400e2a4af47b4ab0b1b9f34397431b30f2f
soykot49 <mdrezwannurrashid26@gmail.com>
2026-02-03 09:48:40 +0600

    new

--- datatypes.py
+++ datatypes.py
@@ -30,3 +30,4 @@ x = b"Hello"	#bytes
 x = bytearray(5)	#bytearray	
 x = memoryview(bytes(5))	#memoryview	
 x = None
+
---
+++ numbers.py
@@ -0,0 +1,14 @@
+# Python Numbers
+# There are three numeric types in Python:
+
+# int
+# float
+# complex
+
+x = 5		# int
+y = 3.14	# float
+z = 2 + 5j	# complex
+
+print(type(x))  
+print(type(y))
+print(type(z))  
